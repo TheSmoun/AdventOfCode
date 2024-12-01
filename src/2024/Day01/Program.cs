f101c21f1163b4ef7deb7087d7e07d5f17312849
Simon Grossmann <simon.grossmann@appgineering.com>
2024-12-01 11:57:43 +0100

    2024 Part 1

--- src/2024/Day01/Program.cs
+++ src/2024/Day01/Program.cs
@@ -1,60 +1,36 @@
-﻿using System.Diagnostics;
-
-#if DEBUG
+﻿#if DEBUG
     var lines = File.ReadAllLines("input.txt");
 #else
     using var reader = new StreamReader(Console.OpenStandardInput(), Console.InputEncoding);
     var lines = reader.ReadToEnd().Split('\n');
 #endif
 
-var sum = lines.Where(l => l.Length > 0).Sum(l =>
+var partList = lines.Select(l => l.Split(' ', StringSplitOptions.RemoveEmptyEntries)).ToList();
+
+var leftNumbers = partList.Select(l => int.Parse(l[0])).ToList();
+leftNumbers.Sort();
+
+var rightNumbers = partList.Select(l => int.Parse(l[1])).ToList();
+rightNumbers.Sort();
+
+var difference = 0;
+for (var i = 0; i < leftNumbers.Count; i++)
 {
-    var firstDigit = l.SkipWhile(c => !char.IsDigit(c)).First() - '0';
-    var lastDigit = l.Reverse().SkipWhile(c => !char.IsDigit(c)).First() - '0';
-    return firstDigit * 10 + lastDigit;
-});
-
-Console.WriteLine("Part 1: " + sum);
-
-var dict = new Dictionary<string, int>
-{
-    { "one", 1 },
-    { "two", 2 },
-    { "three", 3 },
-    { "four", 4 },
-    { "five", 5 },
-    { "six", 6 },
-    { "seven", 7 },
-    { "eight", 8 },
-    { "nine", 9 }
-};
-
-sum = lines.Where(l => l.Length > 0).Sum(l =>
-{
-    var firstDigit = FindFirstDigit(l, 0, 1, dict);
-    var lastDigit = FindFirstDigit(l, l.Length - 1, -1, dict);
-    return firstDigit * 10 + lastDigit;
-});
-
-Console.WriteLine("Part 2: " + sum);
-
-return;
-
-static int FindFirstDigit(string line, int firstIndex, int nextIndexOffset, Dictionary<string, int> dict)
-{
-    for (var i = firstIndex; i >= 0 && i < line.Length; i += nextIndexOffset)
-    {
-        if (char.IsDigit(line[i]))
-            return line[i] - '0';
-
-        var part = line[i..];
-            
-        foreach (var (key, value) in dict)
-        {
-            if (part.StartsWith(key))
-                return value;
-        }
-    }
-
-    throw new UnreachableException();
+    difference += Math.Abs(rightNumbers[i] - leftNumbers[i]);
 }
+
+Console.WriteLine("Part 1: " + difference);
+
+var rightNumberCount = new Dictionary<int, int>();
+foreach (var rightNumber in rightNumbers)
+{
+    rightNumberCount[rightNumber] = rightNumberCount.GetValueOrDefault(rightNumber, 0) + 1;
+}
+
+var similarity = 0;
+foreach (var leftNumber in leftNumbers)
+{
+    similarity += leftNumber * rightNumberCount.GetValueOrDefault(leftNumber, 0);
+}
+
+Console.WriteLine("Part 2: " + similarity);
