be13461d6a23b3e4d7e42f5683db2d01a8041447
Simon Grossmann <simon.grossmann@appgineering.com>
2024-12-07 00:06:14 +0100

    2024 day 6 part 2 brute force.

--- src/2024/Day06/Program.cs
+++ src/2024/Day06/Program.cs
@@ -44,6 +44,9 @@ for (var y = 0; y < lines.Length; y++)
     }
 }
 
+var startPos = pos;
+var startDir = dir;
+
 var guardLines = new List<Line2>();
 var lineStart = pos;
 
@@ -70,6 +73,32 @@ while (IsOnMap(pos, maxX, maxY))
 
 var part1 = guardLines.SelectMany(l => l.GetAllPoints()).ToHashSet().Count;
 
+Console.WriteLine($"Part 1: {part1}");
+
+var part2 = 0;
+
+for (var y = 0; y < maxY; y++)
+{
+    for (var x = 0; x < maxX; x++)
+    {
+        if (map[y, x] || startPos.Equals(new Vec2(x, y)))
+            continue;
+        
+        var mapCopy = new bool[maxY, maxX];
+        Array.Copy(map, mapCopy, mapCopy.Length);
+        
+        mapCopy[y, x] = true;
+
+        if (CheckForLoop(mapCopy, startPos, startDir, maxX, maxY, rotations))
+        {
+            part2++;
+        }
+    }
+}
+
+Console.WriteLine($"Part 2: {part2}");
+
+/* Part 2 with Ray Tracing (not working)
 HashSet<Vec2> part2 = [];
 
 for (var i = 3; i < guardLines.Count; i++)
@@ -87,14 +116,39 @@ for (var i = 3; i < guardLines.Count; i++)
     }
 }
 
-Console.WriteLine($"Part 1: {part1}");
 Console.WriteLine($"Part 2: {part2.Count}");
-
+*/
 return;
 
 static bool IsOnMap(Vec2 pos, int maxX, int maxY)
 {
     return pos.X < maxX && pos.Y < maxY && pos.X >= 0 && pos.Y >= 0;
+}
+
+static bool CheckForLoop(bool[,] map, Vec2 pos, Vec2 dir, int maxX, int maxY, Dictionary<Vec2, Vec2> rotations)
+{
+    HashSet<State> visited = [new(pos, dir)];
+
+    while (IsOnMap(pos, maxX, maxY))
+    {
+        var newPos = pos + dir;
+        if (!IsOnMap(newPos, maxX, maxY))
+            return false;
+    
+        if (map[newPos.Y, newPos.X])
+        {
+            dir = rotations[dir];
+        }
+        else
+        {
+            pos = newPos;
+        }
+
+        if (!visited.Add(new State(pos, dir)))
+            return true;
+    }
+
+    return false;
 }
 
 internal readonly struct Vec2 : IEquatable<Vec2>
@@ -141,6 +195,33 @@ internal readonly struct Vec2 : IEquatable<Vec2>
     public override string ToString()
     {
         return $"({X}, {Y})";
+    }
+}
+
+internal readonly struct State : IEquatable<State>
+{
+    public Vec2 Position { get; }
+    public Vec2 Direction { get; }
+
+    public State(Vec2 position, Vec2 direction)
+    {
+        Position = position;
+        Direction = direction;
+    }
+
+    public bool Equals(State other)
+    {
+        return Position.Equals(other.Position) && Direction.Equals(other.Direction);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is State other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Position, Direction);
     }
 }
 
