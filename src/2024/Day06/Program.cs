ee3229263276bee64c46ab9cff1bd42accc61e6b
Simon Grossmann <simon.grossmann@appgineering.com>
2024-12-08 20:58:46 +0100

    Simplified day 6.

--- src/2024/Day06/Program.cs
+++ src/2024/Day06/Program.cs
@@ -47,45 +47,38 @@ for (var y = 0; y < lines.Length; y++)
 var startPos = pos;
 var startDir = dir;
 
-var guardLines = new List<Line2>();
-var lineStart = pos;
+var positions = new HashSet<Vec2> { startPos };
 
 while (IsOnMap(pos, maxX, maxY))
 {
     var newPos = pos + dir;
     if (!IsOnMap(newPos, maxX, maxY))
-    {
-        guardLines.Add(new Line2(lineStart, pos, dir));
         break;
-    }
     
     if (map[newPos.Y, newPos.X])
     {
-        guardLines.Add(new Line2(lineStart, pos, dir));
-        lineStart = pos;
         dir = rotations[dir];
     }
     else
     {
         pos = newPos;
+        positions.Add(pos);
     }
 }
 
-var guardPositions = guardLines.SelectMany(l => l.GetAllPoints()).ToHashSet();
-
-Console.WriteLine($"Part 1: {guardPositions.Count}");
+Console.WriteLine($"Part 1: {positions.Count}");
 
 var part2 = 0;
 
-foreach (var guardPosition in guardPositions)
+foreach (var position in positions)
 {
-    if (startPos.Equals(guardPosition))
+    if (startPos.Equals(position))
         continue;
     
     var mapCopy = new bool[maxY, maxX];
     Array.Copy(map, mapCopy, mapCopy.Length);
     
-    mapCopy[guardPosition.Y, guardPosition.X] = true;
+    mapCopy[position.Y, position.X] = true;
 
     if (CheckForLoop(mapCopy, startPos, startDir, maxX, maxY, rotations))
     {
@@ -95,26 +88,6 @@ foreach (var guardPosition in guardPositions)
 
 Console.WriteLine($"Part 2: {part2}");
 
-/* Part 2 with Ray Tracing (not working)
-HashSet<Vec2> part2 = [];
-
-for (var i = 3; i < guardLines.Count; i++)
-{
-    var line2 = guardLines[i];
-    
-    for (var j = 0; j < i - 2; j++)
-    {
-        var line1 = guardLines[j];
-        
-        if (line1.TryGetIntersectionWithInfiniteLine(line2, out var intersection) && rotations[line2.Direction].Equals(line1.Direction))
-        {
-            part2.Add(intersection);
-        }
-    }
-}
-
-Console.WriteLine($"Part 2: {part2.Count}");
-*/
 return;
 
 static bool IsOnMap(Vec2 pos, int maxX, int maxY)
@@ -219,72 +192,5 @@ internal readonly struct State : IEquatable<State>
     public override int GetHashCode()
     {
         return HashCode.Combine(Position, Direction);
-    }
-}
-
-internal readonly struct Line2 : IEquatable<Line2>
-{
-    public Vec2 Start { get; }
-    public Vec2 End { get; }
-    public Vec2 Direction { get; }
-
-    public Line2(Vec2 start, Vec2 end, Vec2 direction)
-    {
-        Start = start;
-        End = end;
-        Direction = direction;
-    }
-
-    public IEnumerable<Vec2> GetAllPoints()
-    {
-        yield return Start;
-
-        var pos = Start;
-        while (!pos.Equals(End))
-        {
-            pos += Direction;
-            yield return pos;
-        }
-    }
-    
-    public bool TryGetIntersectionWithInfiniteLine(Line2 segment, out Vec2 intersection)
-    {
-        intersection = default;
-
-        var lineDir = End - Start;
-        var segmentDir = segment.End - segment.Start;
-        var denominator = lineDir * segmentDir;
-
-        if (denominator == 0)
-            return false;
-
-        var startDiff = segment.Start - Start;
-        var t = (float) (startDiff * lineDir) / denominator;
-
-        if (t is < 0 or > 1)
-            return false;
-
-        intersection = new Vec2((int)(segment.Start.X + t * segmentDir.X), (int)(segment.Start.Y + t * segmentDir.Y));
-        return true;
-    }
-
-    public bool Equals(Line2 other)
-    {
-        return Start.Equals(other.Start) && End.Equals(other.End) && Direction.Equals(other.Direction);
-    }
-
-    public override bool Equals(object? obj)
-    {
-        return obj is Line2 other && Equals(other);
-    }
-
-    public override int GetHashCode()
-    {
-        return HashCode.Combine(Start, End, Direction);
-    }
-
-    public override string ToString()
-    {
-        return $"{Start} -> {End} <{Direction}>";
     }
 }
