33b6695dedd18e3d830755bdbe7a3d6f25e9c3f7
Simon Grossmann <simon.grossmann@appgineering.com>
2025-12-02 07:07:35 +0100

    Performance improvements.

--- src/2025/Day02/Program.cs
+++ src/2025/Day02/Program.cs
@@ -1,9 +1,11 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 Console.WriteLine("GO");
 
 #if DEBUG
 var lines = File.ReadAllLines("input.txt");
+var start = Stopwatch.GetTimestamp();
 #else
     using var reader = new StreamReader(Console.OpenStandardInput(), Console.InputEncoding);
     var lines = reader.ReadToEnd().TrimEnd('\n').Split('\n');
@@ -27,32 +29,36 @@ var regex2 = Utils.Part2Pattern();
 
 foreach (var id in ids)
 {
-    if (regex1.IsMatch(id))
+    var s = id.ToString();
+    if (regex1.IsMatch(s))
     {
-        sum1 += long.Parse(id);
+        sum1 += id;
+        sum2 += id;
     }
-
-    if (regex2.IsMatch(id))
+    else if (regex2.IsMatch(s))
     {
-        sum2 += long.Parse(id);
+        sum2 += id;
     }
 }
 
 Console.WriteLine("Part 1: " + sum1);
 Console.WriteLine("Part 2: " + sum2);
 
-return;
+#if DEBUG
+var elapsedTime = Stopwatch.GetElapsedTime(start);
+Console.WriteLine("End: " + elapsedTime);
+#endif
 
 class IdRange
 {
     public long Min { get; set; }
     public long Max { get; set; }
 
-    public IEnumerable<string> GetIds()
+    public IEnumerable<long> GetIds()
     {
         for (var i = Min; i <= Max; i++)
         {
-            yield return $"{i}";
+            yield return i;
         }
     }
 }
